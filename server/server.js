517399e12cbcadafd255197a631b6b8d0c931f2f
ViktorAtanasof <atanasowviktor@gmail.com>
2023-10-10 16:12:29 +0300

    Create basic express.js server to handle the api and different pages

--- server/server.js
+++ server/server.js
@@ -7,22 +7,28 @@ const app = express();
 
 app.use(cors());
 const corsOptions = {
-    origin: "http://localhost:3000"
+  origin: "http://localhost:3000"
 };
-
-const requestEndpoint = "https://greet.bg/wp-json/wc/store/products?page=1";
 
 // This function runs if the http://localhost:5000/getData endpoint
 // is requested with a GET request
 app.get('/getData', cors(corsOptions), async (req, res) => {
-    const fetchOptions = {
-        method: 'GET'
-    }
-    const response = await fetch(requestEndpoint, fetchOptions);
-    const jsonResponse = await response.json();
-    res.json(jsonResponse);
+  const { page } = req.query;
+
+  if (!page) {
+    return res.status(400).json({ error: 'Missing page parameter' });
+  };
+
+  const requestEndpoint = `https://greet.bg/wp-json/wc/store/products?page=${page}`;
+
+  const fetchOptions = {
+    method: 'GET'
+  };
+  const response = await fetch(requestEndpoint, fetchOptions);
+  const jsonResponse = await response.json();
+  res.json(jsonResponse);
 });
 
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
