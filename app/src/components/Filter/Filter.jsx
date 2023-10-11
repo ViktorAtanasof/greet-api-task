c5e6906f912080b3e66e10e4b9e42109e3569163
ViktorAtanasof <atanasowviktor@gmail.com>
2023-10-11 16:12:10 +0300

    Add styles for the Filter component

--- app/src/components/Filter/Filter.jsx
+++ app/src/components/Filter/Filter.jsx
@@ -1,25 +1,30 @@
+import '../Filter/Filter.css';
+
 function Filter({ categories, selectedCategory, onCategoryChange }) {
-    const handleCategoryChange = (e) => {
-      onCategoryChange(e.target.value);
-    };
-  
-    return (
-      <section>
-        <label htmlFor="categoryFilter">Filter by Category:</label>
-        <select
-          id="categoryFilter"
-          onChange={handleCategoryChange}
-          value={selectedCategory}
-        >
-          <option value="">All</option>
-          {categories.map((category) => (
-            <option key={category} value={category}>
-              {decodeURIComponent(category)}
-            </option>
-          ))}
-        </select>
-      </section>
-    );
-  }
+  const handleCategoryChange = (e) => {
+    onCategoryChange(e.target.value);
+  };
+
+  return (
+    <div>
+      <label className="filter-label" htmlFor="categoryFilter">
+        Filter by Category:
+      </label>
+      <select
+        className="filter-select"
+        id="categoryFilter"
+        onChange={handleCategoryChange}
+        value={selectedCategory}
+      >
+        <option value="">All</option>
+        {categories.map((category) => (
+          <option key={category} value={category}>
+            {decodeURIComponent(category)}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
 
 export default Filter;
